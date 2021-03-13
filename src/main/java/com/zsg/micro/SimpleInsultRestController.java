9ed28c5aa3016ea96225d8014b05672a311b1590
hector mendez <hmendez@zobristinc.com>
2021-03-13 10:34:58 -0800

    remove index.html and add SimpleInsultRestController

---
+++ src/main/java/com/zsg/micro/SimpleInsultRestController.java
@@ -0,0 +1,24 @@
+package com.zsg.micro;
+
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleInsultRestController {
+
+	@GetMapping("/api/simple/insult")
+	public String generateInsult() {
+		String words[][] = {{"Artless", "Bawdy", "Beslubbering"}, {"Base-court", "Bat-fowling", "Beef-witted"}, {"Apple-john", "Baggage", "Barnacle"}};
+		String vowels = "AEIOU";
+		String article = "an";
+		String firstAdjective = words[0][new Random().nextInt(words[0].length)];
+		String secondAdjective = words[1][new Random().nextInt(words[1].length)];
+		String noun = words[2][new Random().nextInt(words[2].length)];
+		if (vowels.indexOf(firstAdjective.charAt(0)) == -1) {
+			article = "a";
+		}
+		return String.format("Thou art %s %s %s %s!", article, firstAdjective, secondAdjective, noun);
+	}
+}
